0e495af09eae11065562ad966a0c8cf0ce6cc576
Mariya Ushakova <umm-1986@mail.ru>
2022-09-05 12:56:40 +0300

    Function2HW

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -23,33 +23,59 @@ console.log(result);
 
 
 
-
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
   for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
   return sum;
 }
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    let sum = worker(arrOfArr[i]);
+    let sum = func(arrOfArr[i]);
     if (sum > max) {
       max = sum;
     }
   }
   return max;
 }
-let result = makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker);
+let result2 = makeWork([[1, 2, 3, 4], [10, 20, -10, -20]], worker);
 
-console.log(result)
-
+console.log(result2)
 
 
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let max = -Infinity;
+  let min = Infinity;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  let difference = max - min
+  let modulus = Math.abs(difference);
+  return modulus
 }
+
+function makeWork(arrOfArr, func) {
+  let max = 0;
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let funcResult = func(arrOfArr[i]);
+    if (funcResult > max) {
+      max = funcResult;
+    }
+  }
+  return max;
+}
+let result3 = makeWork([[-10, -20, -40], [10, 20, 30]], worker2);
+
+console.log(result3)
+
+
