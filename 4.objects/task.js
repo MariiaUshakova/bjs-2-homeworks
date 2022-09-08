12ab8ceb229132f8da6587406d18920723f6e8bd
Mariya Ushakova <umm-1986@mail.ru>
2022-09-08 14:23:35 +0300

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,52 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function (...rest) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks = this.marks.concat(rest);
+  }
+}
+
+Student.prototype.getAverage = function (average) {
+  this.average = this.marks.reduce((a, b) => (a + b)) / this.marks.length;
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
+}
+
+student1.setSubject("Algebra");
+student1.addMark(5);
+student1.addMark(4);
+student1.addMark(5);
+student1.addMarks(5, 4, 4)
+console.log(student1.getAverage());
+console.log(student1);
+
+student2.setSubject("Geometry");
+student2.addMark(2);
+student2.addMark(3);
+student2.addMark(2);
+student2.exclude('low grades');
+console.log(student2);
