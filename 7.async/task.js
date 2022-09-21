cf79e66da37e20cd205d29ac120172c20c93615e
Mariya Ushakova <umm-1986@mail.ru>
2022-09-21 20:09:51 +0300

    Async

---
+++ 7.async/task.js
@@ -0,0 +1,102 @@
+class AlarmClock {
+  constructor() {
+    this.alarmCollection = [];
+    this.timerId = null;
+  }
+
+  addClock(time, action, id) {
+    if (id == null) {
+      throw new Error("Невозможно идентифицировать будильник. Параметр id не передан");
+    }
+
+    let foundItems = this.alarmCollection.find(item => {
+      if (item.id === id) {
+        return item;
+      }
+    });
+
+    if (foundItems != undefined) {
+      console.error("Будильник с таким id уже существует");
+      return;
+    }
+
+    this.alarmCollection.push({ id, time, action });
+
+  }
+
+  removeClock(id) {
+    this.alarmCollection = this.alarmCollection.filter(function (item) {
+      return item.id !== id;
+    });
+    if (this.alarmCollection.includes(id) === true) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  getCurrentFormattedTime() {
+    const currentDate = new Date();
+    const hours = currentDate.getHours().toString().padStart(2, "0");
+    const minutes = currentDate.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
+  checkClock(alarm) {
+    if (this.getCurrentFormattedTime() == alarm.time) {
+      alarm.action();
+    }
+  }
+
+  start() {
+
+    const checkAllClocks = () => {
+      this.alarmCollection.forEach(item => {
+        this.checkClock(item);
+      });
+    };
+
+    if (this.timerId == null) {
+      this.timerId = setInterval(checkAllClocks, 1000);
+    }
+
+  }
+
+  stop() {
+    if (this.timerId !== null) {
+      clearInterval(this.timerId);
+      this.timerId = null;
+    }
+  }
+
+
+  printAlarms() {
+    this.alarmCollection.forEach(item => {
+      console.log(item.id + item.time);
+    });
+  }
+
+  clearAlarms() {
+    this.stop();
+    this.alarmCollection.length = 0;
+  }
+
+}
+
+function testCase() {
+
+  let alarmClock = new AlarmClock();
+
+  alarmClock.addClock("20:04", () => { console.log("Доброе утро!") }, 1);
+
+
+  alarmClock.addClock("20:05", () => { console.log("Доброе утро снова!"); alarmClock.removeClock(2) }, 2);
+
+  alarmClock.addClock("20:06", () => { console.log("Доброе утро опять!"); alarmClock.stop(); alarmClock.clearAlarms() }, 3);
+
+  alarmClock.printAlarms();
+
+  alarmClock.start();
+}
+
+testCase();
